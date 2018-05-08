bc0f54350cca57d0e0c30ebe42a7123d206223e1
jmarkow <jmarkow@cns.bu.edu>
2018-05-08 17:07:45 -0400

    Added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='moseq2-viz',
+    author='Jeff Markowitz',
+    description='To boldly go where no mouse has gone before',
+    version='0.02a',
+    platforms=['mac', 'unix'],
+    install_requires=['tqdm', 'matplotlib', 'click', 'ruamel.yaml',
+                      'seaborn'],
+    python_requires='>=3.6',
+    entry_points={'console_scripts': ['moseq2-pca = moseq2_pca.cli:cli']}
+)

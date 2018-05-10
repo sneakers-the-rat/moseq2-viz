e8232f483d82a51235684ca635fecd18c8d8f2ae
jmarkow <jmarkow@cns.bu.edu>
2018-05-10 16:41:28 -0400

    Minor cleanup

--- moseq2_viz/util.py
+++ moseq2_viz/util.py
@@ -83,7 +83,7 @@ def check_video_parameters(index_file):
     }
 
     if 'resolution' in check_parameters:
-        vid_parameters['resolution'] = tuple(dicts[0]['parameters']['resolution'])+100
+        vid_parameters['resolution'] = tuple(dicts[0]['parameters']['resolution'])
 
 
     return vid_parameters

5544bc48ae45b1ff721ed876fbefca5176bd4cf0
jmarkow <jmarkow@cns.bu.edu>
2018-07-12 17:10:59 -0400

    Pinned ruaml.yaml version, 0.15.43 introduced a regression

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='To boldly go where no mouse has gone before',
     version='0.02a',
     platforms=['mac', 'unix'],
-    install_requires=['tqdm', 'matplotlib', 'click', 'ruamel.yaml',
+    install_requires=['tqdm', 'matplotlib', 'click', 'ruamel.yaml<=0.15.0',
                       'seaborn', 'opencv-python', 'pandas', 'networkx'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}

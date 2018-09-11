4ceb09956f98fa68b4c9cefe093c503e76978e73
Jeffrey Markowitz <markowitzmeister@gmail.com>
2018-09-10 23:49:03 -0400

    Relaxed restriction on ruamel.yaml for python 3.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1.1',
     packages=find_packages(),
     platforms=['mac', 'unix'],
-    install_requires=['tqdm', 'matplotlib', 'click', 'ruamel.yaml<=0.15.0',
+    install_requires=['tqdm', 'matplotlib', 'click', 'ruamel.yaml',
                       'seaborn', 'opencv-python', 'pandas', 'networkx'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}

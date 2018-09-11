7019c589efa0eeafdace03d45c98e78f9ebc4ce8
Jeffrey Markowitz <markowitzmeister@gmail.com>
2018-09-11 09:29:01 -0400

    Added numpy to setup.py, pinned version

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(
     version='0.1.1',
     packages=find_packages(),
     platforms=['mac', 'unix'],
-    install_requires=['tqdm', 'matplotlib', 'click', 'ruamel.yaml',
-                      'seaborn', 'opencv-python', 'pandas', 'networkx'],
+    install_requires=['tqdm', 'matplotlib', 'click', 
+                      'ruamel.yaml', 'seaborn', 'opencv-python',
+                      'pandas', 'networkx', 'numpy==1.14.5'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}
 )

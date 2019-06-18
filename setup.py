4ebaa0ddd97d94e8a03f0c5c1335c220a03fe2e5
wingillis <win.gillis@gmail.com>
2019-06-18 16:34:23 +0000

    add the real scikit-learn package to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1.3',
     packages=find_packages(),
     platforms=['mac', 'unix'],
-    install_requires=['tqdm', 'matplotlib', 'click', 'dtaidistance', 'sklearn',
+    install_requires=['tqdm', 'matplotlib', 'click', 'dtaidistance', 'scikit-learn',
                       'ruamel.yaml>=0.15.0', 'seaborn', 'opencv-python',
                       'pandas', 'networkx', 'numpy==1.14.5', 'cytoolz'],
     python_requires='>=3.6',

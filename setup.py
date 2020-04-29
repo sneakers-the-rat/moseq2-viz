b72106b2b6a7d7e5c438300c1e0ce119860c0841
Ayman Zeine <a.zeine.96@gmail.com>
2020-04-29 16:37:46 -0400

    Init new travis stages + locked deps

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
     version='0.2.2',
     packages=find_packages(),
     platforms=['mac', 'unix'],
-    install_requires=['tqdm', 'matplotlib', 'click', 'dtaidistance', 'scikit-learn',
-                      'ruamel.yaml', 'seaborn', 'opencv-python', 'psutil',
-                      'pandas', 'networkx', 'numpy', 'h5py', 'cytoolz'],
+    install_requires=['tqdm==4.40.0', 'matplotlib==3.1.2', 'click==7.0', 'dtaidistance==1.2.3', 'scikit-learn==0.22',
+                      'ruamel.yaml==0.16.5', 'seaborn==0.9.0', 'opencv-python==4.1.2.30', 'psutil==5.6.7',
+                      'pandas==0.25.3', 'networkx==2.4', 'numpy==1.17.4', 'h5py==2.10.0', 'cytoolz==0.10.1'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}
 )

99abc5f8a6c33ca195e9026ddc13c9c9c756be09
Ayman Zeine <a.zeine.96@gmail.com>
2020-07-01 18:33:02 -0400

    downgraded pandas->0.25.3

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     platforms=['mac', 'unix'],
     install_requires=['tqdm==4.40.0', 'matplotlib==3.1.2', 'click==7.0', 'dtaidistance==1.2.3', 'scikit-learn==0.22',
                       'ruamel.yaml==0.16.5', 'seaborn==0.9.0', 'opencv-python==4.1.2.30', 'psutil==5.6.7',
-                      'pandas==1.0.5', 'networkx==2.4', 'numpy==1.18.3', 'h5py==2.10.0', 'cytoolz==0.10.1'],
+                      'pandas==0.25.3', 'networkx==2.4', 'numpy==1.18.3', 'h5py==2.10.0', 'cytoolz==0.10.1'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}
 )

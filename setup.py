a606b297231e95f225e49542b04e02f61f4992fd
Ayman Zeine <a.zeine.96@gmail.com>
2020-09-08 13:39:01 -0400

    Updated dependencies

--- setup.py
+++ setup.py
@@ -35,9 +35,10 @@ setup(
     version=get_version("moseq2_viz/__init__.py"),
     packages=find_packages(),
     platforms=['mac', 'unix'],
-    install_requires=['tqdm==4.40.0', 'matplotlib==3.1.2', 'click==7.0', 'dtaidistance==1.2.3', 'scikit-learn==0.22',
+    install_requires=['tqdm==4.40.0', 'matplotlib==3.1.2', 'click==7.0', 'dtaidistance==1.2.3', 'scikit-learn==0.20.3',
                       'ruamel.yaml==0.16.5', 'seaborn==0.9.0', 'opencv-python==4.1.2.30', 'psutil==5.6.7',
-                      'pandas==0.25.3', 'networkx==2.4', 'numpy==1.18.3', 'h5py==2.10.0', 'cytoolz==0.10.1'],
+                      'pandas==0.25.3', 'networkx==2.4', 'numpy==1.18.3', 'h5py==2.10.0', 'cytoolz==0.10.1',
+                      'qgrid==1.3.1', 'bokeh==2.0.2', 'ipython==7.14.0', 'ipywidgets==7.5.1', 'joblib==0.15.1'],
     python_requires='>=3.6',
     entry_points={'console_scripts': ['moseq2-viz = moseq2_viz.cli:cli']}
 )

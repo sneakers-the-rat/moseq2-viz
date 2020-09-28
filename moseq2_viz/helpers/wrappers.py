09068152c1814f14a5c084725d469fec2ae5fdf9
Ayman Zeine <a.zeine.96@gmail.com>
2020-09-28 19:35:51 -0400

    Separable Crowd Movie Functionality

--- moseq2_viz/helpers/wrappers.py
+++ moseq2_viz/helpers/wrappers.py
@@ -9,6 +9,7 @@ import h5py
 import shutil
 import psutil
 import joblib
+import numpy as np
 from sys import platform
 import ruamel.yaml as yaml
 from tqdm.auto import tqdm
@@ -364,14 +365,14 @@ def plot_transition_graph_wrapper(index_file, model_fit, config_data, output_fil
 
     return plt
 
+
 def make_crowd_movies_wrapper(index_file, model_path, config_data, output_dir):
     '''
+
     Wrapper function to create crowd movie videos and write them to individual
     files depicting respective syllable labels.
-
     Note: function is decorated with function performing initialization operations and saving
     the results in the kwargs variable.
-
     Decorator will retrieve the sorted_index dict and parse the model results into a single dict.
 
     Parameters
@@ -383,7 +384,7 @@ def make_crowd_movies_wrapper(index_file, model_path, config_data, output_dir):
 
     Returns
     -------
-    None
+    cm_paths (dict): Dictionary of syllables and their generated crowd movie paths
     '''
 
     # Load index file and model data
@@ -406,8 +407,8 @@ def make_crowd_movies_wrapper(index_file, model_path, config_data, output_dir):
         label_uuids = model_fit['keys']
 
     # Relabel syllable labels by usage sorting and save the ordering for crowd-movie file naming
-    if config_data['sort']:
-        labels, ordering = relabel_by_usage(labels, count=config_data['count'])
+    if config_data.get('sort', True):
+        labels, ordering = relabel_by_usage(labels, count=config_data.get('count', 'usage'))
     else:
         ordering = list(range(config_data['max_syllable']))
 
@@ -422,11 +423,75 @@ def make_crowd_movies_wrapper(index_file, model_path, config_data, output_dir):
     label_uuids = [uuid for uuid in label_uuids if uuid in uuid_set]
     sorted_index['files'] = {k: v for k, v in sorted_index['files'].items() if k in uuid_set}
 
+    # Get syllable(s) to create crowd movies of
+    if config_data['specific_syllable'] is not None:
+        config_data['crowd_syllables'] = [config_data['specific_syllable']]
+        config_data['max_syllable'] = 1
+    else:
+        config_data['crowd_syllables'] = range(config_data['max_syllable'])
+
     # Write parameter information yaml file in crowd movies directory
-    write_crowd_movie_info_file(model_path=model_path, model_fit=model_fit, index_file=index_file, output_dir=output_dir)
+    write_crowd_movie_info_file(model_path=model_path, model_fit=model_fit, index_file=index_file,
+                                output_dir=output_dir)
 
     # Write movies
     write_crowd_movies(sorted_index, config_data, ordering, labels, label_uuids, output_dir)
+
+    cm_paths = {}
+    if config_data.get('separate_by') == 'groups':
+        # Get the groups to separate the arrays by
+        groups = list(set(model_fit['metadata']['groups']))
+        group_keys = {g: [] for g in groups}
+
+        for i, v in enumerate(sorted_index['files'].values()):
+            group_keys[v['group']].append(i)
+
+        ## Filter these three arrays to get desired crowd movie source
+        for k, v in group_keys.items():
+            group_labels = np.array(labels)[v]
+            group_label_uuids = np.array(label_uuids)[v]
+            group_index = {'files': {k1: v1 for k1, v1 in sorted_index['files'].items() if k1 in group_label_uuids},
+                           'pca_path': sorted_index['pca_path']}
+
+            # create a subdirectory for each group
+            output_subdir = os.path.join(output_dir, k + '/')
+            if not os.path.exists(output_subdir):
+                os.makedirs(output_subdir)
+
+            # Write crowd movie for given group and syllable(s)
+            cm_paths[k] = write_crowd_movies(group_index, config_data, ordering, group_labels, group_label_uuids,
+                                             output_subdir)
+
+    elif config_data.get('separate_by') == 'sessions':
+        # Separate the arrays by session
+        sessions = list(set(model_fit['metadata']['uuids']))
+
+        session_names = {}
+        for i, s in enumerate(sessions):
+            session_name = sorted_index['files'][s]['metadata']['SessionName']
+
+            if session_name in config_data['session_names']:
+                session_names[session_name] = i
+
+        for k, v in session_names.items():
+            session_labels = [np.array(labels)[v]]
+            session_label_uuids = [np.array(label_uuids)[v]]
+            session_index = {'files': {k1: v1 for k1, v1 in sorted_index['files'].items() if k1 in session_label_uuids},
+                             'pca_path': sorted_index['pca_path']}
+
+            # create a subdirectory for each group
+            output_subdir = os.path.join(output_dir, k + '/')
+            if not os.path.exists(output_subdir):
+                os.makedirs(output_subdir)
+
+            # Write crowd movie for given group and syllable(s)
+            cm_paths[k] = write_crowd_movies(session_index, config_data, ordering,
+                                             session_labels, session_label_uuids, output_subdir)
+    else:
+        # Write movies
+        cm_paths = {'all': write_crowd_movies(sorted_index, config_data, ordering, labels, label_uuids, output_dir)}
+
+    return cm_paths
 
 def copy_h5_metadata_to_yaml_wrapper(input_dir, h5_metadata_path):
     '''

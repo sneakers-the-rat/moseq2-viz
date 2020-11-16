bba61cf005bec46c4cc73abf067c940ffeaa6e3c
Winthrop Gillis <win.gillis@gmail.com>
2020-11-15 22:39:22 -0500

    streamline setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,13 @@
 from setuptools import setup, find_packages
-import subprocess
 import codecs
-import os.path
-import sys
-
-
-def install(package):
-    subprocess.call([sys.executable, "-m", "pip", "install", package])
-
-
-try:
-    import cv2
-except ImportError:
-    install('opencv-python')
+import os
 
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
     with codecs.open(os.path.join(here, rel_path), 'r') as fp:
         return fp.read()
+
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
@@ -27,6 +16,7 @@ def get_version(rel_path):
             return line.split(delim)[1]
     else:
         raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='moseq2-viz',

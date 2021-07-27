5b92e6c692eb1a44f191a62a95e8dac7b5e8468b
Ayman Zeine <a.zeine.96@gmail.com>
2021-07-27 14:19:41 -0400

    fix: removed delete statement to next line

--- scripts/download_test_data.sh
+++ scripts/download_test_data.sh
@@ -1,1 +1,2 @@
-curl -L -o data.zip https://www.dropbox.com/sh/u5rr91fe15t7vpt/AAC_ysh-XbXanZDS4XXMcBEva?dl=0 && unzip data.zip -d data && rm data.zip
+curl -L -o data.zip https://www.dropbox.com/sh/u5rr91fe15t7vpt/AAC_ysh-XbXanZDS4XXMcBEva?dl=0 && unzip data.zip -d data
+rm data.zip

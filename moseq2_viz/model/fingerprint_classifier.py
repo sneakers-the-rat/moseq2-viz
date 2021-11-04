c2c28dd15deb1be9624029262009c3b64d70c526
Sherry <sherrylin42@gmail.com>
2021-11-04 17:20:58 -0400

    fix: removing f1 score in the function
    f1 score requires pos_label and average params. Users should compute f1 score themselves using out

--- moseq2_viz/model/fingerprint_classifier.py
+++ moseq2_viz/model/fingerprint_classifier.py
@@ -281,7 +281,6 @@ def classifier_fingerprint(summary, features=['MoSeq'], preprocessor=None, class
             out['shuff_split'].append(split)
 
     out = {k: np.concatenate(v) if 'y' in k else np.array(v) for k, v in out.items()}
-    out['f1'] = f1_score(out['y_true'], out['y_pred'], pos_label='Amphetamine')
     out['accuracy'] = accuracy_score(out['y_true'], out['y_pred'])
     out['shuff_f1'] = f1_score(out['shuff_y_true'], out['shuff_y_pred'], pos_label='Amphetamine')
     out['shuff_accuracy'] = accuracy_score(out['shuff_y_true'], out['shuff_y_pred'])
